34f4c63d82408fba0592dd40d8a8695d2ff2e6f2
Dani-gr <dani.gr@uma.es>
2023-02-11 19:05:03 +0100

    Created PersonTest.java

---
+++ Ejercicio1/src/test/java/org/mps/garoda/person/PersonTest.java
@@ -0,0 +1,17 @@
+package org.mps.garoda.person;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void averageAgePerGender() {
+    }
+}
