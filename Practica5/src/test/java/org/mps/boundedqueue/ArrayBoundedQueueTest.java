b334ea7c1526fafa8a84ed38a04cbe0ef5cf0029
Dani-gr <dani.gr@uma.es>
2023-04-28 03:36:54 +0200

    Reorganized tests and added full queue tests
    Moreover, test instances for checking nullability of elements out-of-bounds have been added.

--- Practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
+++ Practica5/src/test/java/org/mps/boundedqueue/ArrayBoundedQueueTest.java
@@ -21,56 +21,75 @@ class ArrayBoundedQueueTest {
         queue = new ArrayBoundedQueue<>(3);
     }
 
-    @SuppressWarnings("JUnitTestMethodWithNoAssertions") //TODO: Remove
-    @Test
-    @DisplayName("Given a non-positive size when creating a queue, an IllegalArgumentException will be thrown.")
-    void nonPositiveSizeThrowsException() {
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> queue = new ArrayBoundedQueue<>(0))
-                .withMessage("ArrayBoundedException: capacity must be positive");
+    @Nested
+    @DisplayName("Tests for invalid arguments:")
+    class InvalidArguments {
+        @SuppressWarnings("JUnitTestMethodWithNoAssertions") //TODO: Remove
+        @Test
+        @DisplayName("Given a non-positive size when creating a queue, an IllegalArgumentException will be thrown.")
+        void nonPositiveSizeThrowsException() {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> queue = new ArrayBoundedQueue<>(0))
+                    .withMessage("ArrayBoundedException: capacity must be positive");
+        }
+
+        @Test
+        @DisplayName("Given a not-full queue, when putting a null element, an IllegalArgumentException will be thrown.")
+        void puttingNullValueThrowsException() {
+            assertThat(queue.isFull()).as("The queue should not be full").isFalse();
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                    .isThrownBy(() -> queue.put(null))
+                    .withMessage("put: element cannot be null");
+        }
     }
 
-
     @Test
-    @DisplayName("Given a not-full queue, when putting a null element, an IllegalArgumentException will be thrown.")
-    void puttingNullValueThrowsException() {
-        assertThat(queue.isFull()).isFalse().as("The queue should not be full");
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> queue.put(null))
-                .withMessage("put: element cannot be null");
+    @DisplayName("Given a non-empty queue, when the queue is iterated, only not-null elements are iterated through.")
+    void onlyNonNullElementsWhenIterating() {
+        // Setup [1 - 9] (9 -> 1)
+        queue.put(4);
+        queue.put(7);
+        queue.put(9);
+        queue.get();
+        queue.get();
+        queue.put(1);
+
+        assertThat(queue.isEmpty())
+                .as("The queue should not be empty")
+                .isFalse();
+        var iterator = queue.iterator();
+        assertThat(iterator)
+                .as("The iterator should not be null")
+                .isNotNull();
+        iterator.forEachRemaining(element -> assertThat(element)
+                .as("All elements iterated through should not be null")
+                .isNotNull());
     }
 
     @Nested
     @DisplayName("Given an empty queue")
     class emptyQueue {
         @Test
-        @DisplayName("when the buffer is checked, all the elements are null.")
-        void hasAllNullElementsInTheArray() {
+        @DisplayName("when the queue is iterated, there's no elements to iterate through.")
+        void hasNoElementsInTheIterator() {
             assertThat(queue.isEmpty())
                     .as("The queue should be created empty")
                     .isTrue();
-            Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
-            assertThat(buffer).isNotNull().as("The buffer should not be null")
-                    .containsOnlyNulls().as("All elements of the buffer should be null");
-        }
-
-        @Test
-        @DisplayName("when the queue is iterated, there's no elements to iterate through.")
-        void hasNoElementsInTheIterator() {
-            assertThat(queue.isEmpty()).isTrue()
-                    .as("The queue should be created empty");
             var iterator = queue.iterator();
-            assertThat(iterator).isNotNull()
-                    .as("The iterator should not be null");
-            assertThat(iterator.hasNext()).isFalse()
-                    .as("The iterator should not have any elements to iterate from.");
+            assertThat(iterator)
+                    .as("The iterator should not be null")
+                    .isNotNull();
+            assertThat(iterator.hasNext())
+                    .as("The iterator should not have any elements to iterate through.")
+                    .isFalse();
         }
 
         @Test
         @DisplayName("when calling the method get, an EmptyBoundedQueueException is thrown.")
         void getFails() {
-            assertThat(queue.isEmpty()).isTrue()
-                    .as("The queue should be created empty");
+            assertThat(queue.isEmpty())
+                    .as("The queue should be created empty")
+                    .isTrue();
             assertThatExceptionOfType(EmptyBoundedQueueException.class)
                     .isThrownBy(() -> queue.get())
                     .withMessage("get: empty bounded queue");
@@ -88,12 +107,155 @@ class ArrayBoundedQueueTest {
         }
 
         @Test
-        @DisplayName("when the buffer is checked, all the elements are null except por 1.")
-        void hasAllNullElementsButOneInTheArray() {
-            assertThat(queue.isEmpty()).isFalse()
-                    .as("The queue should not be empty");
-            assertThat(queue.isFull()).isFalse()
-                    .as("The queue should not be full");
+        @DisplayName("when the queue is iterated, the number of elements to iterate through is less than the buffer's size.")
+        void hasOnlyQueueElementsInTheIterator() {
+            assertThat(queue.isEmpty())
+                    .as("The queue should not be empty")
+                    .isFalse();
+            assertThat(queue.isFull())
+                    .as("The queue should not be full")
+                    .isFalse();
+
+            var iterator = queue.iterator();
+            Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
+            assertThat(iterator)
+                    .as("The iterator should not be null").isNotNull();
+            assertThat(buffer)
+                    .as("The buffer should not be null").isNotNull();
+
+            int numElements = 0;
+            while (iterator.hasNext()) {
+                iterator.next();
+                numElements++;
+            }
+            assertThat(numElements)
+                    .as("The iterator should iterate through less elements than the buffer's size.")
+                    .isLessThan(buffer.length);
+        }
+    }
+
+    @Nested
+    @DisplayName("Given a full queue")
+    class fullQueue {
+        @BeforeEach
+        void setUp() {
+            queue.put(1);
+            queue.put(2);
+            queue.put(3);
+        }
+
+        @Test
+        @DisplayName("when the queue is iterated, the number of elements to iterate through equals the buffer's size.")
+        void hasAllElementsInTheIterator() {
+            assertThat(queue.isFull())
+                    .as("The queue should be full")
+                    .isTrue();
+
+            var iterator = queue.iterator();
+            Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
+            assertThat(iterator)
+                    .as("The iterator should not be null").isNotNull();
+            assertThat(buffer)
+                    .as("The buffer should not be null").isNotNull();
+
+            int numElements = 0;
+            while (iterator.hasNext()) {
+                iterator.next();
+                numElements++;
+            }
+            assertThat(numElements)
+                    .as("The iterator should iterate through the same number of elements as the buffer's size.")
+                    .isEqualTo(buffer.length);
+        }
+
+        @Test
+        @DisplayName("when calling the method put, a FullBoundedQueueException is thrown.")
+        void putFails() {
+            assertThat(queue.isFull())
+                    .as("The queue should be full")
+                    .isTrue();
+            assertThatExceptionOfType(FullBoundedQueueException.class)
+                    .isThrownBy(() -> queue.put(5))
+                    .withMessage("put: full bounded queue");
+        }
+    }
+
+    /**
+     * An empty queue has already been tested {@link emptyQueue here}.<br/>
+     * A full queue has already been tested {@link fullQueue here}.
+     */
+    @Nested
+    @DisplayName("Given a queue, when the buffer is checked, all the elements are null except for the values in the queue:")
+    class hasAllNullElementsInTheArrayExceptTheOnesPresentInTheQueue {
+        @Test
+        @DisplayName("1. [- - -] () | Empty")
+        void empty() {
+            assertThat(queue.isEmpty())
+                    .as("The queue should be empty")
+                    .isTrue();
+            Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
+            assertThat(buffer).as("The buffer should not be null").isNotNull()
+                    .as("All elements of the buffer should be null").containsOnlyNulls();
+        }
+
+        @Test
+        @DisplayName("2. [- 3 -] (3) | One element")
+        void oneElement() {
+            // Setup
+            queue.put(1);
+            queue.put(3);
+            queue.get();
+
+            check();
+        }
+
+        @Test
+        @DisplayName("3. [5 3 -] (5 -> 3) | Two elements together")
+        void twoElementsTogether() {
+            // Setup
+            queue.put(5);
+            queue.put(3);
+
+            check();
+        }
+
+        @Test
+        @DisplayName("4. [4 - 9] (9 -> 4) | Two elements looping")
+        void twoElementsLooping() {
+            // Setup
+            queue.put(1);
+            queue.put(2);
+            queue.put(9);
+            queue.get();
+            queue.get();
+            queue.put(4);
+
+            check();
+        }
+
+        @Test
+        @DisplayName("5. [4 7 9] (4 -> 7 -> 9) | Full")
+        void full() {
+            // Setup
+            queue.put(4);
+            queue.put(7);
+            queue.put(9);
+
+            assertThat(queue.isFull())
+                    .as("The queue should be full")
+                    .isTrue();
+            Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
+            assertThat(buffer).as("The buffer should not be null").isNotNull()
+                    .as("All elements of the buffer should not be null").doesNotContainNull();
+        }
+
+        private void check() {
+            assertThat(queue.isEmpty())
+                    .as("The queue should not be empty")
+                    .isFalse();
+            assertThat(queue.isFull())
+                    .as("The queue should not be full")
+                    .isFalse();
 
             Object[] buffer = (Object[]) ReflectionTestUtils.getField(queue, "buffer");
             assertThat(buffer).isNotNull().as("The buffer should not be null");
@@ -117,18 +279,37 @@ class ArrayBoundedQueueTest {
                 j = ReflectionTestUtils.invokeMethod(queue, "advance", j);
             }
         }
+    }
 
+    @Nested
+    @DisplayName("Tests for loops:")
+    class Loops {
         @Test
-        @DisplayName("when the queue is iterated, only not-null elements are iterated through.")
-        void onlyOneNonNullElementWhenIterating() {
-            assertThat(queue.isEmpty()).isFalse()
-                    .as("La cola no debería ser vacía");
-            var iterator = queue.iterator();
-            assertThat(iterator).isNotNull()
-                    .as("El iterador no debería ser nulo");
-            iterator.forEachRemaining(element -> assertThat(element)
-                    .as("Todos los elementos del iterador deberían ser no-nulos")
-                    .isNotNull());
+        @DisplayName("Given a non-empty nor-full queue with an element at the end and beginning of the buffer, " +
+                "when getting a value, the *first* attribute becomes 0.")
+        void firstAttributeLoopsAround() {
+            // Setup
+            queue.put(4);
+            queue.put(7);
+            queue.put(9);
+            queue.get();
+            queue.get();
+            queue.put(1);
+
+            // Given
+            assertThat(ReflectionTestUtils.getField(queue, "first"))
+                    .as("The *first* position should be on index 2")
+                    .isEqualTo(2);
+
+            // When
+            queue.get();
+
+            // Then
+            assertThat(ReflectionTestUtils.getField(queue, "first"))
+                    .as("The *first* position should be on index 0")
+                    .isEqualTo(0);
+
         }
     }
+
 }
